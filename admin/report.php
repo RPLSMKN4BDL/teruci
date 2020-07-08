f999de6a27fe523ac8e5032bf8f9726a68dfd377
Faiz Muzaki <34159640+faismuzaki@users.noreply.github.com>
2020-07-08 22:11:55 +0700

    Update report.php

--- admin/report.php
+++ admin/report.php
@@ -1,23 +1,12 @@
-
-<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-<script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-<script>
-    $(document).ready(function(){
-        $('#tabel-data').DataTable();
-    });
-</script>
 <?php
 include 'Core/init.php';
 if(!isset($_SESSION['username']) ){
    header('Location: login.php ');
 }
-?>
-<?Php
 echo "Anda adalah ".$_SESSION['username'];
 ?>
-<a href="logout.php"> Logout</a><br>
-   <h1>Daftar member</h1>
-<table id="tabel-data" class="table table-striped table-bordered" width="100%" cellspacing="0">
+<h1>Daftar member</h1>
+<table id="tabel-data" class="table table-striped table-bordered" width="100%" cellspacing="0" border="1">
 	<thead>
 	<tr>
 	<th>No</th>
@@ -30,26 +19,9 @@ echo "Anda adalah ".$_SESSION['username'];
 	<th>email</th>
 	<th>alamat rumah</th>
 	<th>kota/kabupaten</th>
-	<th>plat nomor</th>
 	<th>Aksi</th>
 	</tr>
 </thead>
-<tfoot>
-	<tr>
-	<th>No</th>
-	<th>Nama Lengkap</th>
-	<th>Nama Panggilan</th>
-	<th>Tempat_lahir</th>
-	<th>Tanggal lahir</th>
-	<th>jenis kelamin</th>
-	<th>nomor hp</th>
-	<th>email</th>
-	<th>alamat rumah</th>
-	<th>kota/kabupaten</th>
-	<th>plat nomor</th>
-	<th>Aksi</th>
-	</tr>
-	</tfoot>
 	 <tbody>
 <tr>
 <?Php
@@ -66,11 +38,10 @@ foreach ($query as $key => $data) { ?>
 	<td><?Php echo $data['email']; ?> </td>
 	<td><?Php echo $data['alamat_rumah']; ?> </td>
 	<td><?Php echo $data['kab_kota']; ?> </td>
-	<td><?Php echo $data['no_pol']; ?> </td>
 	 <td>
-      <a href="edit.php?ID=<?php echo $data['id_teruci']; ?>">Edit</a>
+      <a href="?hal=detail&ID=<?php echo $data['id_teruci']; ?>">Detail</a>
       <input type="hidden" name="ID" >
-      <a href="ubah/delete.php?ID=<?php echo $data['id_teruci']; ?>" name="hapus">Hapus</a>
+      <a href="cetak.php?ID=<?php echo $data['id_teruci']; ?>" target="_blank" >Cetak</a>
       </td>
 </tr>
 <?php } ?>
