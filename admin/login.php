d4c187b39de7a6dd99919642d87ebf38274583bb
Faiz Muzaki <34159640+faismuzaki@users.noreply.github.com>
2020-07-08 22:11:08 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -3,29 +3,60 @@ include 'Core/init.php';
 if(isset($_SESSION['username']) ){
    header('Location: index.php ');
 }
-
 ?>
-<form method="POST">
-	  <label for="inputEmail" class="sr-only">Username</label>
-  <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-  <input type="submit" name="submit">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../aset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../aset/css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" method="POST">
+      <img class="mb-4" src="../aset/gambar/user.jpg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Username</label>
+      <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
 <?php
 if (isset($_POST['submit'])) {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$login = mysqli_query($config, "SELECT * FROM tb_login WHERE username='$username' and password='$password'");
-	$c = mysqli_fetch_array($login);
-	if ($c['username']) {
- 		$_SESSION['username'] = $username;
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $login = mysqli_query($config, "SELECT * FROM tb_login WHERE username='$username' and password='$password'");
+  $c = mysqli_fetch_array($login);
+  if ($c['username']) {
+    $_SESSION['username'] = $username;
  echo("<script>location.href = 'index.php';</script>");
-	}else{
+  }else{
   ?><p class="btn btn-outline-danger">Username/Password Salah </p><?php
-	}
+  }
 
 }
 
 
 ?>
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form>
+  </body>
+</html>
+
