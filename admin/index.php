edc0f672634d0b7fb3774598cc5ca254bf210aa7
Faiz Muzaki <34159640+faismuzaki@users.noreply.github.com>
2020-07-08 22:10:41 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,45 +1,38 @@
+<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+<script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('#tabel-data').DataTable();
+    });
+</script>
 <?php
 include 'Core/init.php';
 if(!isset($_SESSION['username']) ){
    header('Location: login.php ');
 }
-?>
-<?Php
 echo "Anda adalah ".$_SESSION['username'];
 ?>
-<a href="report.php"> Report</a>
+<a href="index.php"> Home</a>
+<a href="?hal=report"> Report</a>
+<a href="?hal=mobil">Mobil</a>
 <a href="logout.php">Logout</a>
-<br>
-   <h1>Daftar member</h1>
-<table border="1">
-<tr>
-	<td>No</td>
-	<td>Nama Lengkap</td>
-	<td>Tempat_lahir</td>
-	<td>Tanggal lahir</td>
-	<td>jenis kelamin</td>
-	<td>nomor hp</td>
-	<td>email</td>
-	<td>alamat rumah</td>
-	<td>kota/kabupaten</td>
-</tr>
-<tr>
-<?Php
-$query = mysqli_query($config, "SELECT * FROM `tb_member` ORDER BY `id_teruci` ASC  ");
-$no = 1;
-foreach ($query as $key => $data) { ?>
-	<td><?Php echo $no++ ?> </td>
-	<td><?Php echo $data['nama_lengkap']; ?> </td>
-	<td><?Php echo $data['tempat_lahir']; ?> </td>
-	<td><?Php echo $data['tgl_lahir']; ?> </td>
-	<td><?Php echo $data['jenis_kelamin']; ?> </td>
-	<td><?Php echo $data['no_hp']; ?> </td>
-	<td><?Php echo $data['email']; ?> </td>
-	<td><?Php echo $data['alamat_rumah']; ?> </td>
-	<td><?Php echo $data['kab_kota']; ?> </td>
 
-</tr>
-<?php } ?>
-</table>
-    <hr class="featurette-divider">
+<?php
+if(empty($_GET['hal'])){
+include "home.php";
+}else if($_GET['hal'] == 'report'){
+include "report.php";
+}else if($_GET['hal'] == 'mobil'){
+include "mobil.php";
+}else if($_GET['hal'] == 'edit'){
+include "edit.php";
+}else if($_GET['hal'] == 'delete'){
+include "delete.php";
+}else if($_GET['hal'] == 'detail'){
+include "detail.php";
+}else if($_GET['hal'] == 'tambah'){
+include "tambah.php";
+}
+?>
+
 
